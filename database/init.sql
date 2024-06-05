fe559da8c4d20be4ed9293ecb64b9b46aafbe2b4
josemanuelcordon <josemanuelcordoncastillo01@gmail.com>
2024-06-05 20:49:04 +0200

    REFACTOR: Cambio de esquema de base de datos

--- database/init.sql
+++ database/init.sql
@@ -3,15 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: database
--- Generation Time: May 07, 2024 at 07:34 PM
--- Server version: 11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+-- Generation Time: Jun 05, 2024 at 06:48 PM
+-- Server version: 11.4.2-MariaDB-ubu2404
 -- PHP Version: 8.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-USE textMe;
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -35,13 +33,6 @@ CREATE TABLE `chat` (
   `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `chat`
---
-
-INSERT INTO `chat` (`id`, `group_chat`, `name`) VALUES
-(1, NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -52,14 +43,6 @@ CREATE TABLE `chat_participants` (
   `id_chat` int(11) NOT NULL,
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `chat_participants`
---
-
-INSERT INTO `chat_participants` (`id_chat`, `id_user`) VALUES
-(1, 4),
-(1, 3);
 
 -- --------------------------------------------------------
 
@@ -75,13 +58,6 @@ CREATE TABLE `message` (
   `chat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `message`
---
-
-INSERT INTO `message` (`id`, `sender`, `content`, `date`, `chat`) VALUES
-(14, 4, 'JUAN JUAN', '2024-06-01 11:26:35', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -93,13 +69,6 @@ CREATE TABLE `message_read` (
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
---
--- Dumping data for table `message_read`
---
-
-INSERT INTO `message_read` (`id_message`, `id_user`) VALUES
-(14, 3);
-
 -- --------------------------------------------------------
 
 --
@@ -110,16 +79,10 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `is_banned` tinyint(1) NOT NULL DEFAULT 0,
+  `active` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(3, 'jose', 'jose@gmail.com', 'vki5ytnr2'),
-(4, 'jose2', 'jose2@gmail.com', 'vki5ytnr2');
 
 --
 -- Indexes for dumped tables
@@ -158,19 +121,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
